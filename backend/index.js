a91060088e91afeb3723fb88ee099e94127eaa5e
zunayed-arp <zunayed.arp@gmail.com>
2023-02-16 21:02:56 +0600

    added server 8000

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
-console.log("Hello from task management tool")
+import express from 'express';
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+const log = msg => console.log(msg);
+
+app.listen(PORT, () => {
+	log(`Server is running at port ${PORT}`);
+});
