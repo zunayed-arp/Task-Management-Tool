cc8a40ca7bd40e4a1192ba3858cffcd3f188ff17
zunayed-arp <zunayed.arp@gmail.com>
2023-02-18 14:57:14 +0600

    authentication and users route added

---
+++ backend/routes/auth.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { register } from "../controllers/auth";
+
+
+const router = express.Router();
+
+
+router.post('register',register);
+router.post('login',login);
+
+
+export default router;
