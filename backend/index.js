236a03fbb60be58d4d3024ccb3573cbb7bb0da39
zunayed-arp <zunayed.arp@gmail.com>
2023-02-18 12:29:00 +0600

    addeed middleware and router

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,30 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./mongo";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import allRoutes from './routes/index';
+
 
 const PORT = process.env.PORT || 8000;
 
 const app = express();
 
-const log = msg => console.log(msg);
+//middlewares
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use("/api",allRoutes);
+
+
+
+export const log = (msg) => console.log(msg);
 
 app.listen(PORT, () => {
-	log(`Server is running at port ${PORT}`);
+  connectDB();
+  log(`Server is running at port ${PORT}`);
 });
