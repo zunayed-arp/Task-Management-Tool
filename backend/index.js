628005990eae8706aff82247a97c79a562424e7c
zunayed-arp <zunayed.arp@gmail.com>
2023-02-21 00:52:03 +0600

    check auth middleware added

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import connectDB from "./mongo";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import allRoutes from "./routes/index";
+import handleError from './utils/errors';
 
 const PORT = process.env.PORT || 8000;
 
@@ -18,6 +19,13 @@ app.use(cookieParser());
 
 //routes
 app.use("/api", allRoutes);
+
+//error
+app.use((err,req,res)=>{
+  const status = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(status).send({'message':message,'stack':err.stack});
+})
 
 // export const log = (msg) => console.log(msg);
 
